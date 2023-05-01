0a1999133043cefed91efa3517d5a00539a5a687
sheng-kai-wang <nssh94879487@gmail.com>
2023-05-01 17:51:44 +0800

    feat: ChatGPT service

--- src/main/java/ntou/soselab/msdobot_llm_lab/Service/ChatGPTService.java
+++ src/main/java/ntou/soselab/msdobot_llm_lab/Service/ChatGPTService.java
@@ -1,5 +1,9 @@
 package ntou.soselab.msdobot_llm_lab.Service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.Gson;
+import com.google.gson.JsonParseException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.configurationprocessor.json.JSONArray;
 import org.springframework.boot.configurationprocessor.json.JSONException;
@@ -16,6 +20,7 @@ import org.springframework.util.FileCopyUtils;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.util.Iterator;
 import java.util.Properties;
 
 @Service
@@ -24,10 +29,10 @@ public class ChatGPTService {
     private final String OPENAI_API_URL;
     private final String OPENAI_API_KEY;
     private final String OPENAI_API_MODEL;
-    private final String PROMPT_INJECTION_DETECTION_URL;
-    private final String END_OF_CAPABILITY_URL;
-    private final String INTENT_CLASSIFICATION_AND_ENTITY_EXTRACTION_URL;
-    private final String QUERYING_MISSING_PARAMETERS_URL;
+    private final String PROMPT_INJECTION_DETECTION_FILE;
+    private final String END_OF_CAPABILITY_FILE;
+    private final String INTENT_CLASSIFICATION_AND_ENTITY_EXTRACTION_FILE;
+    private final String QUERYING_MISSING_PARAMETERS_FILE;
     private final String CAPABILITY_FILE;
 
     @Autowired
@@ -35,18 +40,63 @@ public class ChatGPTService {
         this.OPENAI_API_URL = env.getProperty("openai.api.url");
         this.OPENAI_API_KEY = env.getProperty("openai.api.key");
         this.OPENAI_API_MODEL = env.getProperty("openai.api.model");
-        this.PROMPT_INJECTION_DETECTION_URL = env.getProperty("prompts.prompt_injection_detection.url");
-        this.END_OF_CAPABILITY_URL = env.getProperty("prompts.end_of_capability.url");
-        this.INTENT_CLASSIFICATION_AND_ENTITY_EXTRACTION_URL = env.getProperty("prompts.intent_classification_and_entity_extraction.url");
-        this.QUERYING_MISSING_PARAMETERS_URL = env.getProperty("prompts.querying_missing_parameters.url");
+
+        this.PROMPT_INJECTION_DETECTION_FILE = env.getProperty("prompts.prompt_injection_detection.file");
+        this.END_OF_CAPABILITY_FILE = env.getProperty("prompts.end_of_capability.file");
+        this.INTENT_CLASSIFICATION_AND_ENTITY_EXTRACTION_FILE = env.getProperty("prompts.intent_classification_and_entity_extraction.file");
+        this.QUERYING_MISSING_PARAMETERS_FILE = env.getProperty("prompts.querying_missing_parameters.file");
 
         this.CAPABILITY_FILE = env.getProperty("capability.file");
     }
 
     public boolean isPromptInjection(String userPrompt) {
-        String systemPrompt = systemPromptReader(PROMPT_INJECTION_DETECTION_URL);
+        String systemPrompt = getSystemPrompt(PROMPT_INJECTION_DETECTION_FILE);
         String completion = inference(systemPrompt, userPrompt);
         return completion.contains("true");
+    }
+
+    public boolean isEndOfCapability(String userPrompt) {
+        String systemPrompt = getSystemPrompt(END_OF_CAPABILITY_FILE);
+        String completion = inference(systemPrompt, userPrompt);
+        return completion.contains("true");
+    }
+
+    public JSONObject classifyIntentAndExtractEntity(String userPrompt) throws JsonProcessingException, JsonParseException {
+        String systemPrompt = getSystemPrompt(INTENT_CLASSIFICATION_AND_ENTITY_EXTRACTION_FILE);
+
+        Properties capabilities = getCapabilityYaml();
+        String capabilityJson = new ObjectMapper().writeValueAsString(capabilities);
+
+        systemPrompt = systemPrompt.replace("<CAPABILITY_JSON>", capabilityJson);
+
+        String completion = inference(systemPrompt, userPrompt);
+        return new Gson().fromJson(completion, JSONObject.class);
+    }
+
+    public String queryMissingParameter(String intent, JSONObject providedEntities) throws JSONException {
+        String systemPrompt = getSystemPrompt(QUERYING_MISSING_PARAMETERS_FILE);
+        systemPrompt = systemPrompt.replace("<INTENT_NAME>", intent);
+
+        JSONObject capabilityJSON = new JSONObject(getCapabilityYaml());
+        JSONArray allEntities = capabilityJSON.getJSONArray(intent);
+
+        StringBuilder providedEntityDescription = new StringBuilder();
+        StringBuilder missingEntityDescription = new StringBuilder();
+        for (int i = 0; i < allEntities.length(); i++) {
+            String currentEntityName = allEntities.getString(i);
+            if (providedEntities.has(currentEntityName)) {
+                String entityValue = providedEntities.getString(currentEntityName);
+                providedEntityDescription.append("\"").append(currentEntityName).append("\" as ");
+                providedEntityDescription.append("\"").append(entityValue).append("\", ");
+            } else {
+                missingEntityDescription.append("\"").append(currentEntityName).append("\", ");
+            }
+        }
+
+        systemPrompt = systemPrompt.replace("<PROVIDED_ENTITY_DESCRIPTION>", providedEntityDescription.toString());
+        systemPrompt = systemPrompt.replace("<MISSING_ENTITY_DESCRIPTION>", missingEntityDescription.toString());
+
+        return inference(systemPrompt, null);
     }
 
     private String inference(String systemPrompt, String userPrompt) {
@@ -97,14 +147,14 @@ public class ChatGPTService {
         return restTemplate.postForObject(OPENAI_API_URL, entity, String.class);
     }
 
-    private Properties capabilityReader() {
+    private Properties getCapabilityYaml() {
         YamlPropertiesFactoryBean yaml = new YamlPropertiesFactoryBean();
         yaml.setResources(new ClassPathResource(CAPABILITY_FILE));
         return yaml.getObject();
     }
 
-    private String systemPromptReader(String promptUrl) {
-        ClassPathResource resource = new ClassPathResource(promptUrl);
+    private String getSystemPrompt(String promptFile) {
+        ClassPathResource resource = new ClassPathResource(promptFile);
         byte[] bytes;
         try {
             bytes = FileCopyUtils.copyToByteArray(resource.getInputStream());
--- src/test/java/ntou/soselab/msdobot_llm_lab/MsdoBotLlmLabApplicationTests.java
+++ src/test/java/ntou/soselab/msdobot_llm_lab/MsdoBotLlmLabApplicationTests.java
@@ -1,13 +1,25 @@
 package ntou.soselab.msdobot_llm_lab;
 
+import ntou.soselab.msdobot_llm_lab.Service.ChatGPTService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.env.Environment;
 
 @SpringBootTest
 class MsdoBotLlmLabApplicationTests {
 
+	private Environment env;
+	private ChatGPTService chatGPTService;
+
+	@Autowired
+	public MsdoBotLlmLabApplicationTests(Environment env, ChatGPTService chatGPTService) {
+		this.env = env;
+		this.chatGPTService = chatGPTService;
+	}
+
 	@Test
-	void contextLoads() {
+	void test() {
 	}
 
 }
